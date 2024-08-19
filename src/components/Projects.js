1e59d4046bf45a6f919df25f84f0c826e5f2c3b9
Sri Abarna <154957534+SriAbarnaRavi@users.noreply.github.com>
2024-08-19 17:11:26 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -36,7 +36,7 @@ export default function Projects() {
         <div className='flex flex-col md:flex-row px-10 gap-5'>
                 {config.projects.map((project) => (
                      <div className='relative'>
-                        <img className='h-[200px] w-[500px]' src={project.image}/>
+                        <img className='h-[200px] w-[500px]' src={project.image} alt="projectimage"/>
                         <div className='project-desc'>
                             <p className='text-center px-5 py-5'>{project.description}</p>
                             <div className='flex justify-center'>
